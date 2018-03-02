d1df4cee91b10875b3cf82d8d74f0be45dd8517c
Svendsen92 <ssven16@student.aau.dk>
2018-03-02 13:59:52 +0100

    Add files via upload

--- Library_Code/Lidar/Lidar.py
+++ Library_Code/Lidar/Lidar.py
@@ -1,3 +1,9 @@
+""" 
+This module does not take any arguments. It enables the use of an RPLidar using a breakout board to usb,
+to be pluged into the raspberry pi's usb port. Furthermore it returns the distance measured to an 
+object straigth ahead (through a narrow angel interval).
+"""
+
 import os
 import time
 from rplidar import RPLidar
@@ -7,6 +13,7 @@ PORT_NAME = '/dev/ttyUSB0'
 class lidar:
     
     def __init__(self):
+    	# This function sets the serialport mode to 666 which is necessary to make it work
         try:
             os.system('sudo chmod 666 /dev/ttyUSB0')
             return (True)
@@ -14,20 +21,22 @@ class lidar:
             return (False)
 
     def distance():
+    	# This function returns the distance found by the _measuredDistance function
         _measuredDistance()
         return (dist)
 
                 
-    def _measuredDistance():
-        lidar = RPLidar(PORT_NAME)
-        for measurment in lidar.iter_measurments():
-            line = '\n'.join(str(v) for v in measurment)
-            newline = line.split("\n")
-            if ((float(newline[2]) > 0 and 0.3 > float(newline[2])) or
-                (float(newline[2]) > 359.7 and 360 > float(newline[2]))):
-                dist = float(newline[3]) / 10
-                lidar.stop()
-                lidar.disconnect()
-                time.sleep(0.2)
-                break
+def _measuredDistance():
+	# This function assignates the distance, to an object in a narrow angle interval, to the dist variable 
+    lidar = RPLidar(PORT_NAME)
+    for measurment in lidar.iter_measurments():
+        line = '\n'.join(str(v) for v in measurment)
+        newline = line.split("\n")
+        if ((float(newline[2]) > 0 and 0.3 > float(newline[2])) or
+            (float(newline[2]) > 359.7 and 360 > float(newline[2]))):
+            dist = float(newline[3]) / 10
+            lidar.stop()
+            lidar.disconnect()
+            time.sleep(0.2)
+            break
 
